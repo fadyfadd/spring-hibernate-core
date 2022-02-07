d30790035c69653460500b98c23ba7f37802078f
Fady Faddoul <fady_faddoul@hotmail.com>
2022-02-06 22:00:08 -0600

    miscellaneous features

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -5,9 +5,13 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luv2code.springdemo.entity.Student;
@@ -16,40 +20,71 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	// define endpoint for "/students" - return list of students
+	private List<Student> theStudents;
 	
 	
-	private List<Student> theStudents = null; 
+	// define @PostConstruct to load the student data ... only once!
 	
 	@PostConstruct
 	public void loadData() {
 	
-		theStudents = new ArrayList<Student>(); 
-		
-		theStudents.add(new Student("Poornima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));				
-		 
-	}
-	
-	@GetMapping("/students")	
-	public List<Student> getStudents() {
-
-		return theStudents;
-	}
-	
-	@GetMapping("/students/{studentId}")
-	public Student getStudent(@PathVariable int studentId) {
-
-		List<Student> theStudents = new ArrayList<>();
+		theStudents = new ArrayList<>();
 		
 		theStudents.add(new Student("Poornima", "Patel"));
 		theStudents.add(new Student("Mario", "Rossi"));
 		theStudents.add(new Student("Mary", "Smith"));		
-			
-		return theStudents.get(studentId);
 	}
 	
+	
+	
+	// define endpoint for "/students" - return list of students
+	
+	@GetMapping("/students")
+	public List<Student> getStudents() {
+			
+		return theStudents;
+	}
+	
+	// define endpoint for "/students/{studentId}" - return student at index
+
+	@GetMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId ) {
+		
+		// just index into the list ... keep it simple for now
+		
+		// check the studentId against list size
+		
+		if ( (studentId >= theStudents.size()) || (studentId < 0) ) {			
+			throw new StudentNotFoundException("Student id not found - " + studentId);
+		}
+		
+		return theStudents.get(studentId);
+		
+	}
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+	 }
+
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(Exception exc) {
+		
+		StudentErrorResponse error = new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		error.setMessage(exc.getMessage());
+		error.setTimeStamp(System.currentTimeMillis());
+		
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+	}	
 }
 
 
