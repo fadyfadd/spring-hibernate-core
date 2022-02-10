1746f81298563758c4559fac6fe4db860490c04f
fadyfadd <fady_faddoul@hotmail.com>
2022-02-10 08:32:55 -0500

    spring boot config , actuator

---
+++ 03-actuator-demo/src/test/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplicationTests.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.demo.mycoolapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@JsonTest
+public class MycoolappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
